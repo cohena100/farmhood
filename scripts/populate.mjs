775264f4a020434829bbd3d38179a50e3ece0970
Avi Cohen <cohena100@gmail.com>
2024-06-05 10:31:58 +0300

    Update populate.mjs

--- scripts/populate.mjs
+++ scripts/populate.mjs
@@ -11,47 +11,52 @@ async function main() {
   const parkingLots = await prisma.parkingLot.findMany();
   const users = [
     {
+      id: "0",
       name: "אילנה אביטל",
       phone: "0521234567",
       password: "",
       parkingLotId: parkingLots[getRandomInt(parkingLots.length)].id,
     },
     // {
+    //   id: "1",
     //   name: "גידי גוב",
     //   phone: "0521234567",
     //   password: "",
     //   parkingLotId: parkingLots[getRandomInt(parkingLots.length)].id,
     // },
     // {
+    //   id: "2",
     //   name: "שלמה ארצי",
     //   phone: "0521234567",
     //   password: "",
     //   parkingLotId: parkingLots[getRandomInt(parkingLots.length)].id,
     // },
     {
+      id: "3",
       name: "יהודית רביץ",
       phone: "0521234567",
       password: "",
       parkingLotId: parkingLots[getRandomInt(parkingLots.length)].id,
     },
     {
+      id: "4",
       name: "עפרה חזה",
       phone: "0521234567",
       password: "",
       parkingLotId: parkingLots[getRandomInt(parkingLots.length)].id,
     },
   ];
-  users.forEach(async (user, i) => {
+  users.forEach(async (user) => {
     // await prisma.user.create({
     //   data: {
-    //     id: i.toString(),
-    //     username: i.toString(),
+    //     id: user.id,
+    //     username: user.id,
     //     password: user.password,
     //   },
     // });
     // await prisma.profile.create({
     //   data: {
-    //     id: i.toString(),
+    //     id: user.id,
     //     name: user.name,
     //     phone: user.phone,
     //     parkingLotId: user.parkingLotId,
@@ -70,7 +75,7 @@ async function main() {
     }));
     await prisma.order.create({
       data: {
-        userId: i.toString(),
+        userId: user.id,
         name: user.name,
         phone: user.phone,
         parkingLotId: user.parkingLotId,
